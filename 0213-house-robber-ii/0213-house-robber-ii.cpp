9a8bb254720a2c7f24868e07778ac2c15370d82e
Gurinder Singh <dhawanpriyam7@gmail.com>
2026-07-16 14:53:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (74.21%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    private:
+    int rob1(vector<int>& nums) {
+        
+        // TABULATION METHOD
+
+        int n = nums.size();
+
+        int prev = nums[0];  
+        int prev2 = 0;
+
+        for(int i = 1; i < n; i++){
+
+            int take = nums[i];
+            if(i > 1) take = nums[i] + prev2;
+
+            int not_take = 0 + prev;
+
+            int curr_i = max(take, not_take);
+
+            prev2 = prev;
+            prev = curr_i;
+        }
+        return prev;
+    }
+public:
+    int rob(vector<int>& nums) {
+        
+        vector<int> temp1, temp2;
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+
+        for(int i = 0; i < n; i++){
+            if(i != 0) temp1.push_back(nums[i]);
+
+            if(i != n-1) temp2.push_back(nums[i]);
+        }
+
+        return max(rob1(temp1), rob1(temp2));
+    }
+};
