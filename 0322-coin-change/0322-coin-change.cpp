775db44ec54e6f19bce9a29f1ed95305713ea2e9
Gurinder Singh <dhawanpriyam7@gmail.com>
2026-08-14 23:36:56 +0530

    Time: 49 ms (28.31%), Space: 48.8 MB (41.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,42 @@
 class Solution {
-private:
-    int f(int ind, vector<int>& coins, int target, vector<vector<int>> &dp){
-
-        if(ind == 0){
-
-            if(target % coins[0] == 0) return target/coins[ind];
-
-            else return 1e9;
-        }
-
-        if(dp[ind][target] != -1) return dp[ind][target];
-
-        int notTake = 0 + f(ind - 1, coins, target,dp);
-
-        int take = 1e9;
-        if(coins[ind] <= target){
-            take = 1 + f(ind, coins, target - coins[ind],dp);
-        }
-
-        return dp[ind][target] = min(take, notTake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
+        
         
         int cnt = 0;
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
 
-        int ans = f(n-1,coins, amount, dp);
+        // TABULATION METHOD
+
+        // base case
+        for(int i = 0; i <= amount; i++){
+
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            }
+            else{
+                dp[0][i] = 1e9;
+            }
+        }
+
+        for(int ind=1; ind < n; ind++){
+            for(int target = 0; target <= amount; target++){
+
+                int notTake = 0 + dp[ind - 1][target];
+
+                int take = 1e9;
+                if(coins[ind] <= target){
+                    take = 1 + dp[ind][target - coins[ind]];
+                }
+
+                dp[ind][target] = min(take, notTake);
+
+            }
+        }
+
+        int ans = dp[n-1][amount];
 
         if(ans >= 1e9) return -1;
         else return ans;
