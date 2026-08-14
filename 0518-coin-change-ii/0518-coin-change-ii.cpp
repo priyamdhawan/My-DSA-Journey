c72a0a71a8939d1691bde86b7bef1ba84620b697
Gurinder Singh <dhawanpriyam7@gmail.com>
2026-08-15 03:21:04 +0530

    Time: 23 ms (74.84%), Space: 10.5 MB (81.7%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,37 +3,30 @@ public:
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
-        
-        // FIX: Use 'unsigned int'. It will safely wrap around on massive intermediate 
-        // dead-ends without crashing your program.
-        vector<vector<unsigned int>> dp(n, vector<unsigned int> (amount+1, 0));
+        vector<unsigned int> prev(amount+1, 0), curr(amount+1, 0); 
 
-        // TABULATION METHOD
-
-        // base cases
-        // (Removed dp[0][0] = 1 because the loop handles it when i = 0)
+        prev[0] = 1;
         for(int i = 0; i <= amount; i++){
             if(i % coins[0] == 0){
-                dp[0][i] = 1;
+                prev[i] = 1;
             }
         }
 
         for(int ind = 1; ind < n; ind++){
             for(int target = 0; target <= amount; target++){
 
-                // FIX: Match the 'unsigned int' type
-                unsigned int notTake = dp[ind -1][target];
+                unsigned int notTake = prev[target];
 
                 unsigned int take = 0;
                 if(coins[ind] <= target){
-                    take = dp[ind][target - coins[ind]];
+                    take = curr[target - coins[ind]];
                 }
 
-                dp[ind][target] = take + notTake;
+                curr[target] = take + notTake;
             }
+            prev = curr;
         }
 
-        // It is completely safe to cast back to int at the end!
-        return (int)dp[n-1][amount];
+        return (int)prev[amount];
     }
 };
