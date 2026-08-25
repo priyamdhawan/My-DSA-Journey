b9af6a95941e9a8997a139df576da10233bd6868
Gurinder Singh <dhawanpriyam7@gmail.com>
2026-08-26 01:43:36 +0530

    Time: 11 ms (87.69%), Space: 9.2 MB (82.35%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,47 +1,29 @@
 class Solution {
-private:
-    int f(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-
-        // doing 1 - based indexing for tabulation need
-
-        if(j == 0) return 1;
-        if(i == 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i-1] == t[j-1]){
-            return dp[i][j] = f(i-1, j-1, s, t, dp) + f(i-1, j, s, t, dp);
-        }
-
-        else{
-            return dp[i][j] = f(i-1, j, s, t, dp);
-        }
-    }
 public:
     int numDistinct(string s, string t) {
         
         int n = s.size(), m = t.size();
-        vector<vector<double>> dp(n+1, vector<double>(m+1, -1));
 
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 1;
-        }
-        for(int j = 1; j <= m; j++){  // dont include j = 0 , bcoz it is when dp[i][0] = 1
-            dp[0][j] = 0;
-        }
+        // DOING SPACE OPTIMIZATION
+
+        vector<double> prev(m+1,0), curr(m+1, 0);
+
+        prev[0] = curr[0] = 1;
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
 
                 if(s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 }
 
                 else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+
+            prev = curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
