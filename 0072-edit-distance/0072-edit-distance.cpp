6276fa8e90c6ea0694249e66847ce3dba0655038
Gurinder Singh <dhawanpriyam7@gmail.com>
2026-08-27 01:35:50 +0530

    Time: 0 ms (100%), Space: 10.5 MB (93.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -22,29 +22,31 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
+        
+        // SPACE OPTIMIZATION
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<int> prev(m+1, 0), curr(m+1, 0);
 
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
         for(int j = 0; j <= m; j++){
-            dp[0][j] = j;
+            prev[j] = j; // base case
         }
 
         for(int i = 1; i <= n; i++){
+
+            curr[0] = i; // base case
             for(int j = 1; j <= m; j++){
 
                  if(word1[i-1] == word2[j-1]){
-                    dp[i][j] = 0 + dp[i-1][j-1];
+                    curr[j] = 0 + prev[j-1];
                 }
                 else{
-                    dp[i][j] = 1 + min( dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+                    curr[j] = 1 + min( curr[j-1], min(prev[j], prev[j-1]));
                             //  inserting                deleting                      replacing    -- 3 methods
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
