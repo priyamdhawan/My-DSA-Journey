c6cffd2524804c1c8b46fdd686974b0b87cc4c16
Gurinder Singh <dhawanpriyam7@gmail.com>
2026-09-03 22:43:49 +0530

    Time: 63 ms (61.61%), Space: 14.4 MB (61.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
-    // int f(int ind, int prev, vector<int>& nums, vector<vector<int>> &dp){
-    //     if(ind == nums.size()){
-    //         return 0;
-    //     }
-
-    //     if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
-
-    //     int len = 0 + f(ind+1, prev, nums, dp);
-
-    //     if(prev == -1 || nums[ind] > nums[prev]){
-    //         len = max( len , 1 + f(ind+1, ind, nums, dp));
-    //     }
-
-    //     return dp[ind][prev+1] = len;
-    // }
-    
     int lengthOfLIS(vector<int>& nums) {
-
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        vector<int> dp(n, 1);
+        int maxi = 1;
 
-        // TABULATION METHOD
-        for(int ind = n-1; ind >= 0; ind-- ){
-            for(int prev = ind-1; prev >= -1; prev--){
+        for(int i = 0; i < n; i++){
+            for(int prev = 0; prev < i; prev++){
 
-                int len = 0 + dp[ind+1][prev+1];
-
-                if(prev == -1 || nums[ind] > nums[prev]){
-                    len = max( len , 1 + dp[ind+1][ind+1]);
+                if(nums[i] > nums[prev]){
+                    dp[i] = max(dp[i], 1+ dp[prev]);
                 }
-
-                dp[ind][prev+1] = len;               
             }
-        }
 
-        return dp[0][-1+1];
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
     }
 };
